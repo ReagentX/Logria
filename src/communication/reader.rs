9579d8da9530ce3bb0504a91b202379ca30f9ad1
Christopher Sardegna <gpg@reagentx.net>
2020-11-18 21:44:22 -0500

    Support having a main app loop

--- src/communication/reader.rs
+++ src/communication/reader.rs
@@ -1,15 +1,19 @@
 pub mod main {
+    use ncurses::{getmaxyx, newwin};
     use std::path::Path;
 
+    use crate::communication::input::input_type::InputType;
     use crate::communication::input::stream::{FileInput, InputStream};
     use crate::constants::cli::poll_rate::FASTEST;
+    use crate::ui::interface::build::{command_line, exit_scr, init_scr};
 
+    #[derive(Debug)]
     pub struct LogiraConfig {
         pub poll_rate: u64,    // The rate at which we check for new messages
         smart_poll_rate: bool, // Whether we reduce the poll rate to the message receive speed
         first_run: bool,       // Whether this is a first run or not
-        height: usize,         // Window height
-        width: usize,          // Window width
+        pub height: i32,       // Window height
+        pub width: i32,        // Window width
         loop_time: f64,        // How long a loop of the main app takes
         messages: Option<&'static Vec<String>>, // Default to watching stderr
         previous_messages: Option<&'static Vec<String>>, // Pointer to the previous non-parsed message list, which is continuously updated
@@ -34,7 +38,7 @@ pub mod main {
         insert_mode: bool,              // Default to insert mode (like vim) off
         current_status: String,         // Current status, aka what is in the command line
         highlight_match: bool,          // Determines whether we highlight the match to the user
-        last_row: usize, // The last row we can render, aka number of lines visible in the tty
+        last_row: i32, // The last row we can render, aka number of lines visible in the tty
         stick_to_bottom: bool, // Whether we should follow the stream
         stick_to_top: bool, // Whether we should stick to the top and not render new lines
         manually_controlled_line: bool, // Whether manual scroll is active
@@ -43,10 +47,11 @@ pub mod main {
     }
 
     pub struct MainWindow {
-        pub logria: i32, // fix
         pub config: LogiraConfig,
-        output: Option<i32>, // fix
-        input: Option<i32>, // fix
+        input_type: InputType,
+        stdscr: Option<ncurses::WINDOW>,
+        output: Option<ncurses::WINDOW>, // fix
+        input: Option<ncurses::WINDOW>,  // fix
     }
 
     impl MainWindow {
@@ -70,7 +75,8 @@ pub mod main {
             // Build streams here
             let streams = MainWindow::build_streams(commands);
             MainWindow {
-                logria: 0, // fix
+                stdscr: None,
+                input_type: InputType::Normal,
                 output: None,
                 input: None,
                 config: LogiraConfig {
@@ -105,19 +111,67 @@ pub mod main {
             }
         }
 
+        fn screen(&self) -> ncurses::WINDOW {
+            match self.stdscr {
+                Some(scr) => scr,
+                None => panic!("Attempted to get screen before screen has been initialized!"),
+            }
+        }
+
+        fn output(&self) -> ncurses::WINDOW {
+            match self.output {
+                Some(scr) => scr,
+                None => panic!(
+                    "Attempted to get output window before output window has been initialized!"
+                ),
+            }
+        }
+
+        fn intput(&self) -> ncurses::WINDOW {
+            match self.input {
+                Some(scr) => scr,
+                None => panic!(
+                    "Attempted to get command line before command line has been initialized!"
+                ),
+            }
+        }
+
         pub fn start(&mut self) {
             // Build the UI, get reference to the text body content, etc
+            self.stdscr = Some(init_scr());
+
+            // This is unsafe
+            getmaxyx(
+                self.screen(),
+                &mut self.config.height,
+                &mut self.config.width,
+            );
+            self.config.last_row = self.config.height - 3;
+            // Build output window
+            self.output = Some(newwin(self.config.last_row, self.config.width - 1, 0, 0));
+
+            // Build command line...
+            self.input = Some(command_line(self.screen(), &self.config));
             // Start the main event loop
             self.main();
         }
 
         fn main(&mut self) {
             // Main app loop
+
+            // enum for input mode: {normal, command, regex, choice}
+            // if input mode is command or regex, draw/remove the character to the command line
+            // Otherwise, show status
             loop {
-                break;
-                // Logic goes here...
-                // Need to figure out how to get/fire events between rs-tui and here.
-                // I think events are not working because the text input field is always active
+                match ncurses::getch() {
+                    -1 => continue, // possibly sleep
+                    input => match self.input_type {
+                        InputType::Normal => println!("normal {}", input),
+                        InputType::Command => println!("command {}", input),
+                        InputType::Regex => println!("regex {}", input),
+                        InputType::MultipleChoice => println!("mc {}", input),
+                    }
+                }
             }
         }
     }

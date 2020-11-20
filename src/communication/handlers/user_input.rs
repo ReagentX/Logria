ca455c63ad653b0058d59b0cd15f5300dcd79116
Christopher Sardegna <gpg@reagentx.net>
2020-11-20 11:47:02 -0500

    Support entering text to the command line

--- src/communication/handlers/user_input.rs
+++ src/communication/handlers/user_input.rs
@@ -1,30 +1,111 @@
+use ncurses::{mvwinch, getmaxyx, mv, addch, curs_set, CURSOR_VISIBILITY};
+
 use crate::communication::reader::main::MainWindow;
 use super::handler::HanderMethods;
 
 // Used in Command and Regex handler to capture user typing
 pub struct UserInputHandler {
     x: i32,
-    y: i32
+    y: i32,
+    last_write: i32,
 }
 
 impl UserInputHandler {
 
     /// Get the useable area of the textbox container
-    fn update_dimensions(&self, window: &MainWindow) {
-        ncurses::getmaxyx(window.screen(), &mut self.y, &mut self.x)
+    fn update_dimensions(&mut self, window: &MainWindow) {
+        getmaxyx(window.screen(), &mut self.y, &mut self.x)
     }
 
-    fn validate(&self, key: &i32) -> i32 {
+    /// Get the first and last spots we can write to on the command line
+    fn get_first_last(&self) -> (i32, i32) {
+        (1, self.x())
+    }
+
+    fn y(&self) -> i32 {
+        self.y - 2
+    }
+
+    fn x(&self) -> i32 {
+        self.x - 3
+    }
+
+    fn validate(&self, key: &i32) -> u32 {
         match key {
             127 => 263,  // Ctrl-h to backspace
-            key => *key,
+            key => *key as u32,
         }
     }
 
-    fn do_command(&self, window: &MainWindow, command: char) -> bool {
+    /// Get the first empty x position in the command line
+    fn end_of_line(&mut self, window: &MainWindow) -> i32 {
+        // Ensure we are using the current screen size
+        self.update_dimensions(window);
 
+        // Get the last position we can write to
+        let (_, mut last) = self.get_first_last();
+
+        // Scan right to left
+        loop {
+            // Check for space, aka blank char
+            if mvwinch(window.screen(), self.y(), last) != 32 {
+                return std::cmp::min(last + 1, self.x());
+            }
+
+            // If we are at the first char, we have an empty cli
+            if last == 1 {
+                break
+            }
+
+            last -= 1;
+        }
+        last
     }
-    
+
+    /// Insert character to the input window
+    /// TODO: Support insert vs normal typing mode
+    fn insert_char(&mut self, window: &MainWindow, character: u32) {
+        // Ensure we are using the current screen size
+        self.update_dimensions(window);
+
+        // Last filled position
+        if self.last_write == 0 {
+            self.last_write = self.end_of_line(window);
+        }
+
+        // Handle movement
+        if self.last_write < self.x() {
+            // Move to the last written position
+            mv(self.y(), self.last_write);
+
+            // Insert the char
+            addch(character);
+
+            // Increment the last written position
+            self.last_write += 1;
+        }
+    }
+
+    /// Get the contents of the command line as a string
+    fn gather(&mut self, window: &MainWindow) -> String {
+        let last_char = self.end_of_line(window);
+        let mut out_s = String::from("");
+        for position in 1..last_char {
+            let char_at_pos = mvwinch(window.screen(), self.y(), position);
+            out_s.push(self.get_char(char_at_pos));
+        }
+        curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
+
+        // Reset the last written spot
+        self.last_write = 0;
+        out_s
+    }
+
+    fn do_command(&mut self, window: &MainWindow, command: u32) -> bool {
+        self.end_of_line(window);
+        self.insert_char(&window, command);
+        true
+    }
 }
 
 impl HanderMethods for UserInputHandler {
@@ -32,13 +113,14 @@ impl HanderMethods for UserInputHandler {
         UserInputHandler{
             x: 0,
             y: 0,
+            last_write: 0,
         }
     }
 
-    fn recieve_input(&self, window: &MainWindow, key: i32) {
+    fn recieve_input(&mut self, window: &MainWindow, key: i32) {
+        curs_set(CURSOR_VISIBILITY::CURSOR_VISIBLE);
         let valid_key = self.validate(&key);
-        let character = self.get_char(valid_key);
-        let success = self.do_command(window, character);
-        window.write_to_command_line("got data in CommandHandler")
+        let success = self.do_command(window, valid_key);
+        // window.write_to_command_line("got data in CommandHandler");
     }
 }

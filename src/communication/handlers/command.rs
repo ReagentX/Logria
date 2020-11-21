ecfdba6b3297c072f9e4e844159691dfcc7bf6f5
Christopher Sardegna <gpg@reagentx.net>
2020-11-21 16:22:43 -0500

    Update log

--- src/communication/handlers/command.rs
+++ src/communication/handlers/command.rs
@@ -9,6 +9,6 @@ impl HanderMethods for CommandHandler {
     }
 
     fn recieve_input(&mut self, window: &mut MainWindow, key: i32) {
-        window.write_to_command_line("got data in CommandHandler")
+        window.write_to_command_line(&format!("got data in CommandHandler: {}", key));
     }
 }

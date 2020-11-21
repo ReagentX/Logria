5abb348a2a5243cb7e18d8f3ea40219587af0807
Christopher Sardegna <gpg@reagentx.net>
2020-11-21 11:22:48 -0500

    Implement scrolling

--- src/communication/handlers/normal.rs
+++ src/communication/handlers/normal.rs
@@ -1,20 +1,64 @@
+use std::cmp::{max, min};
+
 use super::handler::HanderMethods;
+use crate::communication::input::input_type::InputType;
 use crate::communication::reader::main::MainWindow;
 
 pub struct NormalHandler {}
 
 impl NormalHandler {
-    fn scroll_up(&self, window: &mut MainWindow) {}
+    fn scroll_up(&self, window: &mut MainWindow) {
+        // TODO: Smart Poll Rate
+        window.config.stick_to_top = false;
+        window.config.stick_to_bottom = false;
+        window.config.manually_controlled_line = true;
 
-    fn scroll_down(&self, window: &mut MainWindow) {}
+        // TODO: handle underflow
+        window.config.current_end = match window.config.current_end.checked_sub(1) {
+            Some(value) => max(value, 1), // No scrolling past the first message
+            None => 1,
+        };
+    }
 
-    fn pg_up(&self, window: &mut MainWindow) {}
+    fn scroll_down(&self, window: &mut MainWindow) {
+        // TODO: Smart Poll Rate
+        window.config.stick_to_top = false;
+        window.config.stick_to_bottom = false;
+        window.config.manually_controlled_line = true;
 
-    fn pg_down(&self, window: &mut MainWindow) {}
+        // Get number of messages we can scroll
+        let num_messages = match window.input_type {
+            InputType::Normal | InputType::MultipleChoice | InputType::Command => {
+                window.messages().len()
+            }
+            InputType::Parser | InputType::Regex => window.config.matched_rows.len(),
+        };
 
-    fn bottom(&self, window: &mut MainWindow) {}
+        // No scrolling past the last message
+        window.config.current_end = min(num_messages, window.config.current_end + 1);
+    }
 
-    fn top(&self, window: &mut MainWindow) {}
+    fn pg_up(&self, window: &mut MainWindow) {
+        (0..window.config.last_row).for_each(|_| {self.scroll_up(window)});
+    }
+
+    fn pg_down(&self, window: &mut MainWindow) {
+        (0..window.config.last_row).for_each(|_| {self.scroll_down(window)});
+    }
+
+    fn bottom(&self, window: &mut MainWindow) {
+        window.write_to_command_line(&format!("got data in NormalHandler: scroll bottom"));
+        window.config.stick_to_top = false;
+        window.config.stick_to_bottom = true;
+        window.config.manually_controlled_line = false;
+    }
+
+    fn top(&self, window: &mut MainWindow) {
+        window.write_to_command_line(&format!("got data in NormalHandler: scroll top"));
+        window.config.stick_to_top = true;
+        window.config.stick_to_bottom = false;
+        window.config.manually_controlled_line = false;
+    }
 
     fn noop(&self) {}
 }
@@ -27,14 +71,14 @@ impl HanderMethods for NormalHandler {
     fn recieve_input(&mut self, window: &mut MainWindow, key: i32) {
         window.write_to_command_line(&format!("got data in NormalHandler: {}", key));
         match key {
-            258 => self.scroll_down(&mut window), // down
-            259 => self.scroll_up(&mut window),   // up
-            260 => self.top(&mut window),         // left
-            261 => self.bottom(&mut window),      // right
-            262 => self.top(&mut window),         // home
-            263 => self.bottom(&mut window),      // end
-            338 => self.pg_down(&mut window),     // pgdn
-            339 => self.pg_up(&mut window),       // pgup
+            258 => self.scroll_down(window), // down
+            259 => self.scroll_up(window),   // up
+            260 => self.top(window),         // left
+            261 => self.bottom(window),      // right
+            262 => self.top(window),         // home
+            263 => self.bottom(window),      // end
+            338 => self.pg_down(window),     // pgdn
+            339 => self.pg_up(window),       // pgup
             _ => self.noop(),
         }
     }

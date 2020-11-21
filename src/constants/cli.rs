810753c6bad40f4d15ad5364748da12c550de516
Christopher Sardegna <gpg@reagentx.net>
2020-11-21 12:21:31 -0500

    Add regex constant

--- src/constants/cli.rs
+++ src/constants/cli.rs
@@ -8,7 +8,7 @@ pub mod poll_rate {
 
 pub mod patterns {
     // TODO: this line need to exist
-    // pub const ANSI_COLOR_PATTERN: &str = "(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]"
+    pub const ANSI_COLOR_PATTERN: &str = r"(\x9B|\x1B\[)[0-?]*[ -/]*[@-~]";
 }
 
 pub mod messages {

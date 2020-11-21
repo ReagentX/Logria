19bc25f1fb405643048edd9711f2745730b8eddb
Christopher Sardegna <gpg@reagentx.net>
2020-11-21 12:21:57 -0500

    Regex handler scaffold

--- src/communication/handlers/regex.rs
+++ src/communication/handlers/regex.rs
@@ -1,14 +1,25 @@
+use regex::Regex;
+
 use crate::communication::reader::main::MainWindow;
+use crate::constants::cli::patterns::ANSI_COLOR_PATTERN;
 use super::handler::HanderMethods;
 
-pub struct RegexHandler {}
+pub struct RegexHandler {
+    color_pattern: Regex, 
+}
+
+impl RegexHandler {
+
+}
 
 impl HanderMethods for RegexHandler {
     fn new() -> RegexHandler {
-        RegexHandler {}
+        RegexHandler {
+            color_pattern: Regex::new(&ANSI_COLOR_PATTERN).unwrap(),
+        }
     }
 
     fn recieve_input(&mut self, window: &mut MainWindow, key: i32) {
-        window.write_to_command_line("got data in RegexHandler")
+        window.write_to_command_line(&format!("got data in RegexHandler: {}", key))
     }
 }

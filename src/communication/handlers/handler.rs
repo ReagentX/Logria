21cc398d45e817054c25f0575675b7091298b3be
Christopher Sardegna <gpg@reagentx.net>
2020-11-24 23:06:33 -0500

    Remove unused struct

--- src/communication/handlers/handler.rs
+++ src/communication/handlers/handler.rs
@@ -3,8 +3,6 @@ use crossterm::event::KeyCode;
 
 use crate::communication::reader::main::MainWindow;
 
-pub struct Handler {}
-
 pub trait HanderMethods {
     fn new() -> Self;
     fn recieve_input(&mut self, window: &mut MainWindow, key: KeyCode) -> Result<()>;

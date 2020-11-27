182cd8b4e5da8ae101dabe045549da7a9e2527f7
Christopher Sardegna <gpg@reagentx.net>
2020-11-27 16:23:19 -0500

    Invert history cli arg so passing disables the tape

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::sync::Arc;
 
 fn main() -> Result<()> {
     let options = util::options::from_command_line();
-    let history = options.is_present("history");
+    let history = !options.is_present("history");
     let smart_poll_rate = options.is_present("smart-poll-rate");
     let exec = options.value_of("exec");
 
@@ -21,6 +21,6 @@ fn main() -> Result<()> {
         
     // Start app
     let mut app = communication::reader::main::MainWindow::new(history, smart_poll_rate);
-    app.start(vec![exec.unwrap_or("README.md").to_string()])?;
+    app.start(vec![exec.unwrap_or("python3 .logria/sample_streams/generate_test_logs_2.py").to_string()])?;
     Ok(())
 }

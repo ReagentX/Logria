e60dd7070b8654ceff67f9856c731adf78e694c4
Christopher Sardegna <gpg@reagentx.net>
2020-11-27 16:24:38 -0500

    Implement history tape

--- src/util/history.rs
+++ src/util/history.rs
@@ -1,13 +1,123 @@
-use std::fs::File;
+use std::cmp::min;
+use std::error::Error;
+use std::fs::OpenOptions;
+use std::io::{BufRead, BufReader, Write};
 
-struct Tape {
+use crate::constants::cli::excludes::HISTORY_EXCLUDES;
+use crate::constants::directories::history_tape;
+
+pub struct Tape {
     history_tape: Vec<String>,
     current_index: usize,
     should_scroll_back: bool,
-    history_tape_file: File,
-    use_cache: bool
 }
 
 impl Tape {
+    pub fn new() -> Tape {
+        let mut tape = Tape {
+            history_tape: vec![],
+            current_index: 0,
+            should_scroll_back: false,
+        };
+        tape.read_from_disk();
+        tape
+    }
 
+    fn read_from_disk(&mut self) {
+        let file = match OpenOptions::new().read(true).open(history_tape()) {
+            // The `description` method of `io::Error` returns a string that describes the error
+            Err(why) => panic!(
+                "Couldn't open {:?}: {}",
+                history_tape(),
+                Error::to_string(&why)
+            ),
+            Ok(file) => file,
+        };
+
+        // Create a buffer and read from it
+        let reader = BufReader::new(file);
+        for line in reader.lines() {
+            if line.is_ok() {
+                self.history_tape.push(line.unwrap());
+            }
+        }
+
+        self.current_index = self.history_tape.len() - 1;
+    }
+
+    pub fn add_item(&mut self, item: &String) {
+        let clean_item = item.trim();
+        if !HISTORY_EXCLUDES.contains(&clean_item) {
+            // Write to internal buffer
+            self.history_tape.push(String::from(clean_item));
+
+            // Reset tape to end
+            self.should_scroll_back = false;
+            self.current_index = self.history_tape.len() - 1;
+
+            // Write to file
+            let mut file = match OpenOptions::new()
+                .read(true)
+                .append(true)
+                .open(history_tape())
+            {
+                // The `description` method of `io::Error` returns a string that describes the error
+                Err(why) => panic!(
+                    "Couldn't open {:?}: {}",
+                    history_tape(),
+                    Error::to_string(&why)
+                ),
+                Ok(file) => file,
+            };
+            match writeln!(file, "{}", format!("{}", clean_item)) {
+                Ok(_) => {}
+                Err(why) => panic!(
+                    "Couldn't write to {:?}: {}",
+                    history_tape(),
+                    Error::to_string(&why)
+                ),
+            }
+        }
+    }
+
+    /// Rewind the tape if possible
+    fn scroll_back_n(&mut self, num_to_scroll: usize) {
+        if !self.history_tape.is_empty() {
+            if self.should_scroll_back {
+                self.current_index = self.current_index.checked_sub(num_to_scroll).unwrap_or(0);
+            } else {
+                self.should_scroll_back = true
+            }
+        }
+    }
+
+    /// Scroll the tape forward if possible
+    fn scroll_forward_n(&mut self, num_to_scroll: usize) {
+        if self.current_index != self.history_tape.len() - 1 {
+            if !self.history_tape.is_empty() {
+                self.current_index = min(
+                    self.history_tape.len() - 1,
+                    self.current_index
+                        .checked_add(num_to_scroll)
+                        .unwrap_or(self.history_tape.len() - 1),
+                );
+            }
+        }
+    }
+
+    /// Common case where we scroll back a single item
+    pub fn scroll_back(&mut self) -> String {
+        self.scroll_back_n(1);
+        self.get_current_item()
+    }
+
+    /// Common case where we scroll up a single item
+    pub fn scroll_forward(&mut self) -> String {
+        self.scroll_forward_n(1);
+        self.get_current_item()
+    }
+
+    pub fn get_current_item(&self) -> String {
+        self.history_tape[self.current_index].clone()
+    }
 }

ba4725190fc9d351725cc2d3059a75df53d8dc64
Christopher Sardegna <gpg@reagentx.net>
2021-03-14 14:13:15 -0400

    Add note to readme

--- src/main.rs
+++ src/main.rs
@@ -22,8 +22,9 @@ fn main() -> Result<()> {
         
     // Start app
     let mut app = communication::reader::main::MainWindow::new(history, smart_poll_rate);
-    app.start(
-        Some(vec![exec.unwrap_or("python3 .logria/sample_streams/generate_test_logs_2.py").to_string()])
-    )?;
+    // app.start(
+    //     Some(vec![exec.unwrap_or("python3 .logria/sample_streams/generate_test_logs_2.py").to_string()])
+    // )?;
+    app.start(None)?;
     Ok(())
 }

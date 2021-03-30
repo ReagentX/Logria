896ff7c864e392c5ed3d7ff4906ffb2a5b24fefb
Christopher Sardegna <gpg@reagentx.net>
2021-03-29 23:49:47 -0400

    Get regex and split parsers working

--- src/communication/handlers/parser.rs
+++ src/communication/handlers/parser.rs
@@ -1,7 +1,5 @@
-use std::{result, usize};
-
 use crossterm::{event::KeyCode, Result};
-use regex::{Error, Regex};
+use regex::Regex;
 
 use super::{handler::HanderMethods, processor::ProcessorMethods};
 use crate::{
@@ -18,37 +16,44 @@ pub struct ParserHandler {
 }
 
 impl ParserHandler {
-    /// Setup the parser
+    /// Setup the parser instance on the main window
     fn setup_parser(&self, window: &mut MainWindow) {
         // TODO: Make this work
         window.config.parser = Some(Parser::load("fake_name"));
     }
 
     /// Parse a message with the current parser rules
-    fn parse(&self, parser: &Parser, index: usize, message: &str) -> result::Result<String, Error> {
+    fn parse(&self, parser: &Parser, index: usize, message: &str) -> Option<String> {
         match parser.pattern_type {
             PatternType::Regex => match Regex::new(&parser.pattern) {
-                Ok(pattern) => Ok(self.regex_handle(message, index, pattern)),
-                Err(why) => Err(why),
+                Ok(pattern) => self.regex_handle(message, index, pattern),
+                Err(_) => None,
             },
-            PatternType::Split => Ok(self.split_handle(message, index, &parser.pattern)),
+            PatternType::Split => self.split_handle(message, index, &parser.pattern),
         }
     }
 
     /// Parse message with regex logic
-    fn regex_handle(&self, message: &str, index: usize, pattern: Regex) -> String {
-        let result: Vec<&str> = pattern.split(message).collect();
-        result[index].to_owned()
+    fn regex_handle(&self, message: &str, index: usize, pattern: Regex) -> Option<String> {
+        if let Some(captures) = pattern.captures(message) {
+            Some(captures.get(index).unwrap().as_str().to_owned())
+        } else {
+            None
+        }
     }
 
     /// Parse message with split logic
-    fn split_handle(&self, message: &str, index: usize, pattern: &str) -> String {
-        let result: Vec<&str> = message.split(pattern).collect();
-        result[index].to_owned()
+    fn split_handle(&self, message: &str, index: usize, pattern: &str) -> Option<String> {
+        let result: Vec<&str> = message.split_terminator(pattern).collect();
+        match result.get(index) {
+            Some(part) => Some(String::from(*part)),
+            None => None,
+        }
     }
 }
 
 impl ProcessorMethods for ParserHandler {
+    /// Return the app to a normal input state
     fn return_to_normal(&mut self, window: &mut MainWindow) -> Result<()> {
         self.clear_matches(window)?;
         window.input_type = Normal;
@@ -57,17 +62,17 @@ impl ProcessorMethods for ParserHandler {
         Ok(())
     }
 
+    /// Clear the parsed messages from the message buffer
     fn clear_matches(&mut self, window: &mut MainWindow) -> Result<()> {
+        // TODO: Determine if regex while parsing still works after parser deactivation
         window.config.parser = None;
-        window.config.regex_pattern = None;
         window.config.parsed_messages.clear();
         window.config.last_index_processed = 0;
-        window.config.highlight_match = false;
         window.reset_command_line()?;
         Ok(())
     }
 
-    /// Process matches, loading the buffer of indexes to matched messages in the main buffer
+    /// Parse messages, loading the buffer of parsed messages in the main window
     fn process_matches(&self, window: &mut MainWindow) {
         // TODO: Possibly async? Possibly loading indicator for large jobs?
         match &window.config.parser {
@@ -78,21 +83,12 @@ impl ProcessorMethods for ParserHandler {
                 // Iterate "forever", skipping to the start and taking up till end-start
                 // TODO: Something to indicate progress
                 for index in (0..).skip(buf_range.0).take(buf_range.1 - buf_range.0) {
-                    match self.parse(
+                    if let Some(message) = self.parse(
                         parser,
                         window.config.parser_index,
                         &window.messages()[index],
                     ) {
-                        Ok(message) => {
-                            window.config.parsed_messages.push(message);
-                        }
-                        Err(why) => {
-                            window
-                                .config
-                                .parsed_messages
-                                .push(format!("Unable to parse message: {:?}", why));
-                            break;
-                        }
+                        window.config.parsed_messages.push(message);
                     }
 
                     // Update the last spot so we know where to start next time
@@ -126,10 +122,111 @@ impl HanderMethods for ParserHandler {
             KeyCode::PageUp => scroll::pg_down(window),
             KeyCode::PageDown => scroll::pg_up(window),
 
+            // Build new parser
+            KeyCode::Char('p') => {
+                self.setup_parser(window);
+            }
+
+            KeyCode::Char('z') => {
+                self.return_to_normal(window)?;
+            }
+
             // Return to normal
             KeyCode::Esc => self.return_to_normal(window)?,
             key => self.input_handler.recieve_input(window, key)?,
         }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod regex_tests {
+    use super::ParserHandler;
+
+    use crate::{
+        communication::{
+            handlers::{handler::HanderMethods, processor::ProcessorMethods},
+            input::input_type::InputType,
+            reader::main::MainWindow,
+        },
+        extensions::parser::Parser,
+    };
+
+    #[test]
+    fn test_can_setup_with_session_first_index() {
+        // Update window config
+        let mut logria = MainWindow::_new_dummy();
+        let handler = ParserHandler::new();
+        let parser = Parser::load("Digit Test");
+
+        logria.config.parser = Some(parser);
+        logria.input_type = InputType::Parser;
+        logria.config.parser_index = 0;
+
+        handler.process_matches(&mut logria);
+
+        assert_eq!(logria.config.parsed_messages[0..10], vec!["1", "2", "3", "4", "5", "6", "7", "8", "9", "1"]);
+    }
+
+    #[test]
+    fn test_can_setup_with_session_second_index() {
+        let mut logria = MainWindow::_new_dummy();
+        let handler = ParserHandler::new();
+        let parser = Parser::load("Digit Test");
+
+        // Update window config
+        logria.config.parser = Some(parser);
+        logria.input_type = InputType::Parser;
+        logria.config.parser_index = 1;
+
+        handler.process_matches(&mut logria);
+
+        assert_eq!(logria.config.parsed_messages[0..10], vec!["1", "2", "3", "4", "5", "6", "7", "8", "9", "1"]);
+    }
+}
+
+#[cfg(test)]
+mod split_tests {
+    use super::ParserHandler;
+
+    use crate::{
+        communication::{
+            handlers::{handler::HanderMethods, processor::ProcessorMethods},
+            input::input_type::InputType,
+            reader::main::MainWindow,
+        },
+        extensions::parser::Parser,
+    };
+
+    #[test]
+    fn test_can_setup_with_session_first_index() {
+        // Update window config
+        let mut logria = MainWindow::_new_dummy();
+        let handler = ParserHandler::new();
+        let parser = Parser::load("Char Test");
+
+        logria.config.parser = Some(parser);
+        logria.input_type = InputType::Parser;
+        logria.config.parser_index = 0;
+
+        handler.process_matches(&mut logria);
+        assert_eq!(logria.config.parsed_messages[0..10], vec!["0", "", "2", "3", "4", "5", "6", "7", "8", "9"]);
+        assert_eq!(logria.config.parsed_messages[15..25], vec!["", "", "", "", "", "20", "2", "22", "23", "24"]);
+    }
+
+    #[test]
+    fn test_can_setup_with_session_second_index() {
+        let mut logria = MainWindow::_new_dummy();
+        let handler = ParserHandler::new();
+        let parser = Parser::load("Char Test");
+
+        // Update window config
+        logria.config.parser = Some(parser);
+        logria.input_type = InputType::Parser;
+        logria.config.parser_index = 1;
+
+        handler.process_matches(&mut logria);
+
+        assert_eq!(logria.config.parsed_messages[0..10], vec!["0", "", "2", "3", "4", "5", "6", "7", "8", "9"]);
     }
 }

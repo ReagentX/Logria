9d26f261cf558aaa46196f0177bab146b7d592e3
Christopher Sardegna <gpg@reagentx.net>
2021-07-05 20:35:19 -0400

    Make smart poll rate cli arg behave properly

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use std::sync::Arc;
 fn main() -> Result<()> {
     let options = util::options::from_command_line();
     let history = !options.is_present("history");
-    let smart_poll_rate = options.is_present("smart-poll-rate");
+    let smart_poll_rate = !options.is_present("smart-poll-rate");
     let exec: Option<Vec<String>> = match options.value_of("exec") {
         Some(text) => Some(vec![text.to_string()]),
         None => None,

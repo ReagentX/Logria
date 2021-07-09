f48ea6790913a78f48e8b8ef94e0c40997a97436
Christopher Sardegna <gpg@reagentx.net>
2021-07-09 19:58:54 -0400

    Add math module

---
+++ src/util/math.rs
@@ -0,0 +1,103 @@
+use std::collections::vec_deque::VecDeque;
+
+#[derive(Debug)]
+pub struct MeanTrack {
+    pub deque: VecDeque<u64>,
+    sum: u64,
+    size: u64,
+    max_size: usize, // Avoids casting `capacity` every insertion
+}
+
+impl MeanTrack {
+    pub fn new(max_size: usize) -> MeanTrack {
+        MeanTrack {
+            deque: VecDeque::with_capacity(max_size),
+            sum: 0,
+            size: 0,
+            max_size,
+        }
+    }
+
+    pub fn update(&mut self, item: u64) {
+        if self.deque.len() >= self.max_size {
+            self.sum -= self.deque.pop_back().unwrap();
+        } else {
+            self.size += 1
+        }
+        self.deque.push_front(item);
+        self.sum += item;
+    }
+
+    pub fn mean(&self) -> u64 {
+        self.sum / self.size
+    }
+
+    pub fn reset(&mut self) {
+        self.sum = 0;
+        self.size = 0;
+        self.deque.clear();
+    }
+}
+
+mod mean_track_tests {
+    use crate::util::math::MeanTrack;
+
+    #[test]
+    fn can_create() {
+        let tracker = MeanTrack::new(5);
+        assert_eq!(tracker.sum, 0);
+        assert_eq!(tracker.max_size, 5);
+        assert_eq!(tracker.size, 0);
+    }
+
+    #[test]
+    fn cant_exceed_capacity() {
+        let mut tracker = MeanTrack::new(2);
+        tracker.update(1);
+        tracker.update(2);
+        tracker.update(3);
+        tracker.update(4);
+        assert_eq!(tracker.sum, 7);
+        assert_eq!(tracker.deque.len(), 2);
+        assert_eq!(tracker.size, 2);
+    }
+
+    #[test]
+    fn can_get_mean_full() {
+        let mut tracker = MeanTrack::new(5);
+        tracker.update(1);
+        tracker.update(2);
+        tracker.update(3);
+        tracker.update(4);
+        tracker.update(5);
+        assert_eq!(tracker.sum, 15);
+        assert_eq!(tracker.mean(), 3);
+        assert_eq!(tracker.size, 5);
+    }
+
+    #[test]
+    fn can_get_mean_under() {
+        let mut tracker = MeanTrack::new(5);
+        tracker.update(1);
+        tracker.update(2);
+        tracker.update(3);
+        assert_eq!(tracker.sum, 6);
+        assert_eq!(tracker.mean(), 2);
+        assert_eq!(tracker.size, 3);
+    }
+
+    #[test]
+    fn can_get_mean_over() {
+        let mut tracker = MeanTrack::new(5);
+        tracker.update(1);
+        tracker.update(2);
+        tracker.update(3);
+        tracker.update(4);
+        tracker.update(5);
+        tracker.update(6);
+        tracker.update(7);
+        assert_eq!(tracker.sum, 25);
+        assert_eq!(tracker.mean(), 5);
+        assert_eq!(tracker.size, 5);
+    }
+}
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -3,3 +3,4 @@ pub mod sanitizers;
 pub mod history;
 pub mod error;
 pub mod types;
+pub mod math;

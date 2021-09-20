9d8c159af8260af0a65d86b02b5d60cf105f51f0
Christopher Sardegna <gpg@reagentx.net>
2021-09-20 00:19:35 -0400

    Scaffold out methods

--- src/util/counter.rs
+++ src/util/counter.rs
@@ -1,8 +1,15 @@
 use std::collections::HashMap;
 use std::hash::Hash;
 
+/// Counter struct inspired by Python's stdlib Counter class
+/// https://github.com/python/cpython/blob/main/Lib/collections/__init__.py
 struct Counter<T> {
     state: HashMap<T, usize>,
+}
+
+struct Item {
+    value: String,
+    count: usize,
 }
 
 impl<T: Hash> Counter<T> {
@@ -11,6 +18,25 @@ impl<T: Hash> Counter<T> {
             state: HashMap::new(),
         }
     }
+
+    /// Determine the total number of items in the Counter
+    fn total(&self) -> usize {
+        self.state.values().into_iter().sum()
+    }
+
+    /// Get the `n` most common items in the Counter
+    fn most_common(&self, n: usize) -> Vec<Item> {
+        vec![]
+    }
+
+    /// Increment an item into the counter, creating if it does not exist
+    fn increment(&self, item: T) {}
+
+    /// Reduce an item from the counter, removing if it becomes 0
+    fn decrement(&self, item: T) {}
+
+    /// Remove an item from the counter
+    fn delete(&self, item: T) {}
 }
 
 #[cfg(test)]

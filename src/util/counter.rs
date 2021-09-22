fbf18592cfd1927995504c685e94dc38ac994e57
Christopher Sardegna <gpg@reagentx.net>
2021-09-21 23:57:47 -0400

    Implement most of the Counter struct

--- src/util/counter.rs
+++ src/util/counter.rs
@@ -1,50 +1,342 @@
-use std::collections::HashMap;
 use std::hash::Hash;
+use std::{
+    cmp::{Eq, PartialEq},
+    collections::HashMap,
+    fmt::Display,
+};
 
 /// Counter struct inspired by Python's stdlib Counter class
-/// https://github.com/python/cpython/blob/main/Lib/collections/__init__.py
 struct Counter<T> {
-    state: HashMap<T, usize>,
+    state: HashMap<T, u64>,
+    order: HashMap<u64, Vec<T>>,
 }
 
-struct Item {
-    value: String,
-    count: usize,
-}
-
-impl<T: Hash> Counter<T> {
+impl<T: Hash + Eq + PartialEq + Clone + Display> Counter<T> {
     fn new() -> Counter<T> {
         Counter {
             state: HashMap::new(),
+            order: HashMap::new(),
         }
     }
 
     /// Determine the total number of items in the Counter
-    fn total(&self) -> usize {
+    fn total(&self) -> u64 {
         self.state.values().into_iter().sum()
     }
 
     /// Get the `n` most common items in the Counter
-    fn most_common(&self, n: usize) -> Vec<Item> {
-        vec![]
+    fn most_common(&self, n: usize) -> Vec<String> {
+        // Place to store the result
+        let mut result = Vec::with_capacity(n);
+        if n == 0 {
+            return result;
+        }
+
+        // Keep track of how many items we have added
+        let mut total_added = 0;
+
+        // Get the keys sorted from highest to lowest
+        let mut counts: Vec<u64> = self
+            .order
+            .keys()
+            .into_iter()
+            .map(|f| f.to_owned())
+            .collect();
+        counts.sort_unstable();
+        // panic!("{:?}", counts);
+
+        // Get the value under each key
+        for count in counts.iter().rev() {
+            let items = self.order.get(count).unwrap();
+            for item in items {
+                result.push(format!("{}: {}", item, count));
+                total_added += 1;
+                if total_added == n {
+                    return result;
+                }
+            }
+        }
+
+        result
+    }
+
+    /// Remove an item from the internal order store
+    fn purge_from_order(&mut self, item: &T, count: &u64) {
+        if let Some(order) = self.order.get_mut(count) {
+            // If there was data there, remove the existing item
+            if !order.is_empty() {
+                order.retain(|i| i != item);
+                if order.is_empty() {
+                    self.order.remove(count);
+                }
+            };
+        };
+    }
+
+    fn purge_from_state(&mut self, item: &T) {
+        self.state.remove(item);
+    }
+
+    /// Update the internal item order HashMap
+    fn update_order(&mut self, item: T, old_count: &u64, new_count: &u64) {
+        self.purge_from_order(&item, old_count);
+        match self.order.get_mut(new_count) {
+            Some(v) => {
+                v.push(item);
+            }
+            None => {
+                self.order.insert(*new_count, vec![item]);
+            }
+        }
     }
 
     /// Increment an item into the counter, creating if it does not exist
-    fn increment(&self, item: T) {}
+    fn increment(&mut self, item: T) {
+        let old_count = self.state.get(&item).unwrap_or(&0).to_owned();
+        let new_count = old_count.checked_add(1);
+        match new_count {
+            Some(count) => self.state.insert(item.to_owned(), count),
+            None => self.state.insert(item.to_owned(), old_count),
+        };
+        self.update_order(item, &old_count, &new_count.unwrap_or(old_count));
+    }
 
     /// Reduce an item from the counter, removing if it becomes 0
-    fn decrement(&self, item: T) {}
+    fn decrement(&mut self, item: T) {
+        let old_count = self.state.get(&item).unwrap_or(&0).to_owned();
+        let new_count = old_count.checked_sub(1);
+        match new_count {
+            Some(count) => {
+                if count > 0 {
+                    self.state.insert(item.to_owned(), count);
+                    self.update_order(item, &old_count, &count);
+                } else {
+                    self.delete(&item);
+                }
+            }
+            None => {
+                self.delete(&item);
+            }
+        };
+    }
 
-    /// Remove an item from the counter
-    fn delete(&self, item: T) {}
+    /// Remove an item from the counter completely
+    fn delete(&mut self, item: &T) {
+        // TODO: Make this safe
+        let count = self.state.get(item).unwrap().to_owned();
+        self.purge_from_order(item, &count);
+        self.purge_from_state(item);
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+
     use super::Counter;
+
+    static A: &str = "a";
+    static B: &str = "b";
+    static C: &str = "c";
+    static D: &str = "d";
 
     #[test]
     fn can_construct_counter() {
         let c: Counter<String> = Counter::new();
+    }
+
+    #[test]
+    fn can_count() {
+        let mut c: Counter<String> = Counter::new();
+        c.increment(A.to_owned());
+        c.increment(A.to_owned());
+        c.increment(A.to_owned());
+        c.increment(B.to_owned());
+        c.increment(B.to_owned());
+
+        let mut expected_count = HashMap::new();
+        expected_count.insert(A.to_owned(), 3);
+        expected_count.insert(B.to_owned(), 2);
+
+        let mut expected_order: HashMap<u64, Vec<String>> = HashMap::new();
+        expected_order.insert(3, vec![A.to_owned()]);
+        expected_order.insert(2, vec![B.to_owned()]);
+
+        assert_eq!(c.state, expected_count);
+        assert_eq!(c.order, expected_order);
+    }
+
+    #[test]
+    fn can_sum() {
+        let mut c: Counter<String> = Counter::new();
+        c.increment(A.to_owned());
+        c.increment(A.to_owned());
+        c.increment(A.to_owned());
+        c.increment(B.to_owned());
+        c.increment(B.to_owned());
+
+        let mut expected = HashMap::new();
+        expected.insert(A.to_owned(), 3);
+        expected.insert(B.to_owned(), 2);
+
+        assert_eq!(c.total(), 5);
+    }
+
+    #[test]
+    fn can_decrement() {
+        let mut c: Counter<String> = Counter::new();
+        c.increment(A.to_owned());
+        c.increment(A.to_owned());
+        c.increment(A.to_owned());
+        c.increment(B.to_owned());
+        c.increment(B.to_owned());
+        c.decrement(A.to_owned());
+
+        let mut expected_count = HashMap::new();
+        expected_count.insert(A.to_owned(), 2);
+        expected_count.insert(B.to_owned(), 2);
+
+        let mut expected_order: HashMap<u64, Vec<String>> = HashMap::new();
+        expected_order.insert(2, vec![B.to_owned(), A.to_owned()]);
+
+        assert_eq!(c.state, expected_count);
+        assert_eq!(c.order, expected_order);
+    }
+
+    #[test]
+    fn can_decrement_auto_remove() {
+        let mut c: Counter<String> = Counter::new();
+        c.increment(A.to_owned());
+        c.increment(B.to_owned());
+        c.increment(B.to_owned());
+        c.decrement(A.to_owned());
+
+        let mut expected_count = HashMap::new();
+        expected_count.insert(B.to_owned(), 2);
+
+        let mut expected_order: HashMap<u64, Vec<String>> = HashMap::new();
+        expected_order.insert(2, vec![B.to_owned()]);
+
+        assert_eq!(c.state, expected_count);
+        assert_eq!(c.order, expected_order);
+    }
+
+    #[test]
+    fn can_delete() {
+        let mut c: Counter<String> = Counter::new();
+        c.increment(A.to_owned());
+        c.increment(A.to_owned());
+        c.increment(A.to_owned());
+        c.increment(B.to_owned());
+        c.increment(B.to_owned());
+        c.delete(&A.to_owned());
+
+        let mut expected_count = HashMap::new();
+        expected_count.insert(B.to_owned(), 2);
+
+        let mut expected_order: HashMap<u64, Vec<String>> = HashMap::new();
+        expected_order.insert(2, vec![B.to_owned()]);
+
+        assert_eq!(c.state, expected_count);
+        assert_eq!(c.order, expected_order);
+    }
+
+    #[test]
+    fn can_get_top_0() {
+        let mut c: Counter<String> = Counter::new();
+        c.increment(A.to_owned());
+        c.increment(A.to_owned());
+        c.increment(A.to_owned());
+        c.increment(B.to_owned());
+        c.increment(B.to_owned());
+        c.increment(B.to_owned());
+        c.increment(C.to_owned());
+        c.increment(C.to_owned());
+        c.increment(D.to_owned());
+
+        let expected: Vec<String> = vec![];
+
+        assert_eq!(c.most_common(0), expected);
+    }
+
+    #[test]
+    fn can_get_top_1() {
+        let mut c: Counter<String> = Counter::new();
+        c.increment(A.to_owned());
+        c.increment(A.to_owned());
+        c.increment(A.to_owned());
+        c.increment(B.to_owned());
+        c.increment(B.to_owned());
+        c.increment(B.to_owned());
+        c.increment(C.to_owned());
+        c.increment(C.to_owned());
+        c.increment(D.to_owned());
+
+        let expected = vec![String::from("a: 3")];
+
+        assert_eq!(c.most_common(1), expected);
+    }
+
+    #[test]
+    fn can_get_top_2() {
+        let mut c: Counter<String> = Counter::new();
+        c.increment(A.to_owned());
+        c.increment(A.to_owned());
+        c.increment(A.to_owned());
+        c.increment(B.to_owned());
+        c.increment(B.to_owned());
+        c.increment(B.to_owned());
+        c.increment(C.to_owned());
+        c.increment(C.to_owned());
+        c.increment(D.to_owned());
+
+        let expected = vec![String::from("a: 3"), String::from("b: 3")];
+
+        assert_eq!(c.most_common(2), expected);
+    }
+
+    #[test]
+    fn can_get_top_3() {
+        let mut c: Counter<String> = Counter::new();
+        c.increment(A.to_owned());
+        c.increment(A.to_owned());
+        c.increment(A.to_owned());
+        c.increment(B.to_owned());
+        c.increment(B.to_owned());
+        c.increment(B.to_owned());
+        c.increment(C.to_owned());
+        c.increment(C.to_owned());
+        c.increment(D.to_owned());
+
+        let expected = vec![
+            String::from("a: 3"),
+            String::from("b: 3"),
+            String::from("c: 2"),
+        ];
+
+        assert_eq!(c.most_common(3), expected);
+    }
+
+    #[test]
+    fn can_get_top_4() {
+        let mut c: Counter<String> = Counter::new();
+        c.increment(A.to_owned());
+        c.increment(A.to_owned());
+        c.increment(A.to_owned());
+        c.increment(B.to_owned());
+        c.increment(B.to_owned());
+        c.increment(B.to_owned());
+        c.increment(C.to_owned());
+        c.increment(C.to_owned());
+        c.increment(D.to_owned());
+
+        let expected = vec![
+            String::from("a: 3"),
+            String::from("b: 3"),
+            String::from("c: 2"),
+            String::from("d: 1"),
+        ];
+
+        assert_eq!(c.most_common(4), expected);
     }
 }

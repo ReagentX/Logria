1f85b988ebb1f159b10ce7972d43fb6b3504443c
Christopher Sardegna <gpg@reagentx.net>
2021-09-26 18:02:46 -0400

    Make update return a result on success

--- src/util/aggregators/aggregator.rs
+++ src/util/aggregators/aggregator.rs
@@ -1,15 +1,16 @@
+use crate::util::error::LogriaError;
 use serde::{Deserialize, Serialize};
 
 pub trait Aggregator<T> {
     fn new(method: &AggregationMethod) -> Self;
-    fn update(&mut self, message: T);
+    fn update(&mut self, message: T) -> Result<(), LogriaError>;
     fn messages(&self, n: usize) -> Vec<String>;
 }
 
 #[derive(Eq, PartialEq, Serialize, Deserialize, Debug)]
 pub enum AggregationMethod {
     Mean,
-    Mode,  // Special case of Count, for most_common(1)
+    Mode, // Special case of Count, for most_common(1)
     Sum,
     Count,
     Date(String),

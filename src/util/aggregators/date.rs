2f85510c636ec9dcc61d7e19dae281d992fedc9d
Christopher Sardegna <gpg@reagentx.net>
2021-09-26 18:21:34 -0400

    Add message logic and tests

--- src/util/aggregators/date.rs
+++ src/util/aggregators/date.rs
@@ -86,12 +86,25 @@ impl Aggregator<String> for Date {
     }
 
     fn messages(&self, _: usize) -> Vec<String> {
-        vec![
-            format!("Rate: {:.4} {}", self.rate, self.unit),
-            format!("Count: {}", self.count),
-            format!("Earliest: {}", self.earliest),
-            format!("Latest: {}", self.latest),
-        ]
+        let mut out_v = vec![
+            format!("    Rate: {:.4} {}", self.rate, self.unit),
+            format!("    Count: {}", self.count),
+        ];
+        match self.parser_type {
+            ParserType::Date => {
+                out_v.push(format!("    Earliest: {}", self.earliest.date()));
+                out_v.push(format!("    Latest: {}", self.latest.date()));
+            }
+            ParserType::Time => {
+                out_v.push(format!("    Earliest: {}", self.earliest.time()));
+                out_v.push(format!("    Latest: {}", self.latest.time()));
+            }
+            ParserType::DateTime => {
+                out_v.push(format!("    Earliest: {}", self.earliest));
+                out_v.push(format!("    Latest: {}", self.latest));
+            }
+        };
+        out_v
     }
 }
 
@@ -148,10 +161,10 @@ mod use_tests {
     #[test]
     fn can_update_date() {
         let mut d: Date = Date::new(&AggregationMethod::Date("[month]/[day]/[year]".to_string()));
-        d.update("01/01/2021".to_string());
-        d.update("01/02/2021".to_string());
-        d.update("01/03/2021".to_string());
-        d.update("01/04/2021".to_string());
+        d.update("01/01/2021".to_string()).unwrap();
+        d.update("01/02/2021".to_string()).unwrap();
+        d.update("01/03/2021".to_string()).unwrap();
+        d.update("01/04/2021".to_string()).unwrap();
 
         let expected = Date {
             format: "[month]/[day]/[year]".to_string(),
@@ -176,10 +189,10 @@ mod use_tests {
         let mut d: Date = Date::new(&AggregationMethod::Time(
             "[hour]:[minute]:[second]".to_string(),
         ));
-        d.update("01:01:00".to_string());
-        d.update("02:01:00".to_string());
-        d.update("03:01:00".to_string());
-        d.update("04:01:00".to_string());
+        d.update("01:01:00".to_string()).unwrap();
+        d.update("02:01:00".to_string()).unwrap();
+        d.update("03:01:00".to_string()).unwrap();
+        d.update("04:01:00".to_string()).unwrap();
 
         let expected = Date {
             format: "[hour]:[minute]:[second]".to_string(),
@@ -204,10 +217,10 @@ mod use_tests {
         let mut d: Date = Date::new(&AggregationMethod::DateTime(
             "[month]/[day]/[year] [hour]:[minute]:[second]".to_string(),
         ));
-        d.update("01/01/2021 01:01:00".to_string());
-        d.update("01/02/2021 02:01:00".to_string());
-        d.update("01/03/2021 03:01:00".to_string());
-        d.update("01/04/2021 04:01:00".to_string());
+        d.update("01/01/2021 01:01:00".to_string()).unwrap();
+        d.update("01/02/2021 02:01:00".to_string()).unwrap();
+        d.update("01/03/2021 03:01:00".to_string()).unwrap();
+        d.update("01/04/2021 04:01:00".to_string()).unwrap();
 
         let expected = Date {
             format: "[month]/[day]/[year] [hour]:[minute]:[second]".to_string(),
@@ -231,6 +244,75 @@ mod use_tests {
         assert_eq!(d.count, expected.count);
         assert_eq!(d.unit, expected.unit);
         assert_eq!(d.rate, expected.rate);
+    }
+}
+
+#[cfg(test)]
+mod message_tests {
+    use crate::util::aggregators::{
+        aggregator::{AggregationMethod, Aggregator},
+        date::Date,
+    };
+
+    #[test]
+    fn can_update_date() {
+        let mut d: Date = Date::new(&AggregationMethod::Date("[month]/[day]/[year]".to_string()));
+        d.update("01/01/2021".to_string()).unwrap();
+        d.update("01/02/2021".to_string()).unwrap();
+        d.update("01/03/2021".to_string()).unwrap();
+        d.update("01/04/2021".to_string()).unwrap();
+
+        let expected = vec![
+            "    Rate: 1 per day".to_string(),
+            "    Count: 4".to_string(),
+            "    Earliest: 2021-01-01".to_string(),
+            "    Latest: 2021-01-04".to_string(),
+        ];
+        let messages = d.messages(1);
+
+        assert_eq!(messages, expected);
+    }
+
+    #[test]
+    fn can_update_time() {
+        let mut d: Date = Date::new(&AggregationMethod::Time(
+            "[hour]:[minute]:[second]".to_string(),
+        ));
+        d.update("01:01:00".to_string()).unwrap();
+        d.update("02:01:00".to_string()).unwrap();
+        d.update("03:01:00".to_string()).unwrap();
+        d.update("04:01:00".to_string()).unwrap();
+
+        let expected = vec![
+            "    Rate: 1 per hour".to_string(),
+            "    Count: 4".to_string(),
+            "    Earliest: 1:01:00.0".to_string(),
+            "    Latest: 4:01:00.0".to_string(),
+        ];
+        let messages = d.messages(1);
+
+        assert_eq!(messages, expected);
+    }
+
+    #[test]
+    fn can_update_date_time() {
+        let mut d: Date = Date::new(&AggregationMethod::DateTime(
+            "[month]/[day]/[year] [hour]:[minute]:[second]".to_string(),
+        ));
+        d.update("01/01/2021 01:01:00".to_string()).unwrap();
+        d.update("01/02/2021 02:01:00".to_string()).unwrap();
+        d.update("01/03/2021 03:01:00".to_string()).unwrap();
+        d.update("01/04/2021 04:01:00".to_string()).unwrap();
+
+        let expected = vec![
+            "    Rate: 1 per day".to_string(),
+            "    Count: 4".to_string(),
+            "    Earliest: 2021-01-01 1:01:00.0".to_string(),
+            "    Latest: 2021-01-04 4:01:00.0".to_string(),
+        ];
+        let messages = d.messages(1);
+
+        assert_eq!(messages, expected);
     }
 }
 

1b65e2c8ef2bc7b848e8eddea06e6292fc1f8458
Christopher Sardegna <gpg@reagentx.net>
2021-09-27 00:22:56 -0400

    Use single type, move new out of Aggregator impl

--- src/util/aggregators/mean.rs
+++ src/util/aggregators/mean.rs
@@ -1,75 +1,23 @@
-use std::{fmt::Display, ops::AddAssign};
+use crate::util::{aggregators::aggregator::Aggregator, error::LogriaError};
 
-use num_traits::{one, zero, Float, PrimInt};
-
-use crate::util::{
-    aggregators::aggregator::{AggregationMethod, Aggregator},
-    error::LogriaError,
-};
-
-pub struct IntMean<T: PrimInt + Display> {
-    count: T,
-    total: T,
-}
-
-/// Integer implementation of Mean
-impl<I: PrimInt + Display> Aggregator<I> for IntMean<I> {
-    fn new(_: &AggregationMethod) -> IntMean<I> {
-        IntMean {
-            count: zero(),
-            total: zero(),
-        }
-    }
-
-    fn update(&mut self, message: I) -> Result<(), LogriaError> {
-        self.count = self.count.checked_add(&one()).unwrap_or_else(I::max_value);
-        self.total = self
-            .total
-            .checked_add(&message)
-            .unwrap_or_else(I::max_value);
-        Ok(())
-    }
-
-    fn messages(&self, _: usize) -> Vec<String> {
-        vec![
-            format!("    Mean: {}", self.mean()),
-            format!("    Count: {}", self.count),
-            format!("    Total: {}", self.total),
-        ]
-    }
-}
-
-impl<I: PrimInt + Display> IntMean<I> {
-    fn mean(&self) -> I {
-        self.total.checked_div(&self.count).unwrap_or_else(zero)
-    }
-}
-
-struct FloatMean<F: Float + AddAssign + Display> {
-    count: F,
-    total: F,
+pub struct Mean {
+    count: f64,
+    total: f64,
 }
 
 /// Float implementation of Mean
-impl<F: Float + AddAssign + Display> Aggregator<F> for FloatMean<F> {
-    fn new(_: &AggregationMethod) -> FloatMean<F> {
-        FloatMean {
-            count: zero::<F>(),
-            total: zero::<F>(),
-        }
-    }
-
-    fn update(&mut self, message: F) -> Result<(), LogriaError> {
-        if self.count >= F::max_value() {
-            self.count = F::max_value()
+impl Aggregator for Mean {
+    fn update(&mut self, message: &str) -> Result<(), LogriaError> {
+        if self.count >= f64::MAX {
+            self.count = f64::MAX;
         } else {
-            self.count += one::<F>();
+            self.count += 1.;
         };
 
-        if self.total >= F::max_value() {
-            self.total = F::max_value()
+        if self.total >= f64::MAX {
+            self.total = f64::MAX;
         } else {
-            self.total += message
+            self.total += self.parse(message);
         };
 
         Ok(())
@@ -84,9 +32,20 @@ impl<F: Float + AddAssign + Display> Aggregator<F> for FloatMean<F> {
     }
 }
 
-impl<F: Float + AddAssign + Display> FloatMean<F> {
-    fn mean(&self) -> F {
-        if self.count == zero::<F>() {
+impl Mean {
+    pub fn new() -> Mean {
+        Mean {
+            count: 0.,
+            total: 0.,
+        }
+    }
+
+    fn parse(&self, message: &str) -> f64 {
+        todo!()
+    }
+
+    fn mean(&self) -> f64 {
+        if self.count == 0. {
             self.total
         } else {
             self.total / self.count
@@ -95,75 +54,15 @@ impl<F: Float + AddAssign + Display> FloatMean<F> {
 }
 
 #[cfg(test)]
-mod int_tests {
-    use crate::util::aggregators::{
-        aggregator::{AggregationMethod::Mean, Aggregator},
-        mean::IntMean,
-    };
-
-    #[test]
-    fn mean() {
-        let mut mean: IntMean<i32> = IntMean::new(&Mean);
-        mean.update(1).unwrap();
-        mean.update(2).unwrap();
-        mean.update(3).unwrap();
-
-        assert_eq!(mean.mean(), 2);
-        assert_eq!(mean.total, 6);
-        assert_eq!(mean.count, 3);
-    }
-
-    #[test]
-    fn display() {
-        let mut mean: IntMean<i32> = IntMean::new(&Mean);
-        mean.update(1).unwrap();
-        mean.update(2).unwrap();
-        mean.update(3).unwrap();
-
-        assert_eq!(
-            mean.messages(1),
-            vec![
-                "    Mean: 2".to_string(),
-                "    Count: 3".to_string(),
-                "    Total: 6".to_string(),
-            ]
-        );
-    }
-
-    #[test]
-    fn empty_mean() {
-        let mean: IntMean<i8> = IntMean::new(&Mean);
-
-        assert_eq!(mean.mean(), 0);
-        assert_eq!(mean.total, 0);
-        assert_eq!(mean.count, 0);
-    }
-
-    #[test]
-    fn mean_overflow() {
-        let mut mean: IntMean<i32> = IntMean::new(&Mean);
-        mean.update(i32::MAX - 1).unwrap();
-        mean.update(i32::MAX - 1).unwrap();
-
-        assert_eq!(mean.mean(), i32::MAX / 2);
-        assert_eq!(mean.total, i32::MAX);
-        assert_eq!(mean.count, 2);
-    }
-}
-
-#[cfg(test)]
 mod float_tests {
-    use crate::util::aggregators::{
-        aggregator::{AggregationMethod::Mean, Aggregator},
-        mean::FloatMean,
-    };
+    use crate::util::aggregators::{aggregator::Aggregator, mean::Mean};
 
     #[test]
     fn mean() {
-        let mut mean: FloatMean<f64> = FloatMean::new(&Mean);
-        mean.update(1_f64).unwrap();
-        mean.update(2_f64).unwrap();
-        mean.update(3_f64).unwrap();
+        let mut mean: Mean = Mean::new();
+        mean.update("1_f64").unwrap();
+        mean.update("2_f64").unwrap();
+        mean.update("3_f64").unwrap();
 
         assert!((mean.mean() - 2_f64).abs() == 0_f64);
         assert!((mean.total - 6_f64).abs() == 0_f64);
@@ -172,10 +71,10 @@ mod float_tests {
 
     #[test]
     fn display() {
-        let mut mean: FloatMean<f64> = FloatMean::new(&Mean);
-        mean.update(1_f64).unwrap();
-        mean.update(2_f64).unwrap();
-        mean.update(3_f64).unwrap();
+        let mut mean: Mean = Mean::new();
+        mean.update("1_f64").unwrap();
+        mean.update("2_f64").unwrap();
+        mean.update("3_f64").unwrap();
 
         assert_eq!(
             mean.messages(1),
@@ -189,18 +88,18 @@ mod float_tests {
 
     #[test]
     fn empty_mean() {
-        let mean: FloatMean<f32> = FloatMean::new(&Mean);
+        let mean: Mean = Mean::new();
 
-        assert!(mean.mean() == 0_f32);
-        assert!(mean.total == 0_f32);
-        assert!(mean.count == 0_f32);
+        assert!(mean.mean() == 0_f64);
+        assert!(mean.total == 0_f64);
+        assert!(mean.count == 0_f64);
     }
 
     #[test]
     fn mean_overflow() {
-        let mut mean: FloatMean<f64> = FloatMean::new(&Mean);
-        mean.update(f64::MAX - 1_f64).unwrap();
-        mean.update(f64::MAX - 1_f64).unwrap();
+        let mut mean: Mean = Mean::new();
+        mean.update(&format!("{}test", f64::MAX - 1_f64)).unwrap();
+        mean.update(&format!("{} test", f64::MAX - 1_f64)).unwrap();
 
         assert!((mean.mean() - f64::MAX / 2_f64).abs() == 0_f64);
         assert!((mean.total - f64::MAX).abs() == 0_f64);
--- src/util/aggregators/sum.rs
+++ src/util/aggregators/sum.rs
@@ -1,49 +1,19 @@
-use num_traits::{zero, Float, PrimInt};
-use std::{fmt::Display, ops::AddAssign};
-
 use crate::util::{
     aggregators::aggregator::{AggregationMethod, Aggregator},
     error::LogriaError,
 };
 
-/// Integer sum implementation
-pub struct IntSum<I: AddAssign + Display + PrimInt> {
-    total: I,
+pub struct Sum {
+    total: f64,
 }
 
-impl<I: AddAssign + Display + PrimInt> Aggregator<I> for IntSum<I> {
-    fn new(_: &AggregationMethod) -> Self {
-        IntSum { total: zero() }
-    }
-
-    fn update(&mut self, message: I) -> Result<(), LogriaError> {
-        self.total = self
-            .total
-            .checked_add(&message)
-            .unwrap_or_else(I::max_value);
-        Ok(())
-    }
-
-    fn messages(&self, _: usize) -> Vec<String> {
-        vec![format!("    Total: {}", self.total)]
-    }
-}
-
-/// Flaot sum implementation
-pub struct FloatSum<F: AddAssign + Display + Float> {
-    total: F,
-}
-
-impl<F: AddAssign + Display + Float> Aggregator<F> for FloatSum<F> {
-    fn new(_: &AggregationMethod) -> Self {
-        FloatSum { total: zero() }
-    }
-
-    fn update(&mut self, message: F) -> Result<(), LogriaError> {
-        if self.total >= F::max_value() {
-            self.total = F::max_value()
+impl Aggregator for Sum {
+    fn update(&mut self, message: &str) -> Result<(), LogriaError> {
+        // TODO: Parse message
+        if self.total >= f64::MAX {
+            self.total = f64::MAX;
         } else {
-            self.total += message
+            self.total += self.parse(message);
         };
         Ok(())
     }
@@ -53,90 +23,52 @@ impl<F: AddAssign + Display + Float> Aggregator<F> for FloatSum<F> {
     }
 }
 
-#[cfg(test)]
-mod int_tests {
-    use crate::util::aggregators::{
-        aggregator::{AggregationMethod::Sum, Aggregator},
-        sum::IntSum,
-    };
-
-    #[test]
-    fn sum() {
-        let mut sum: IntSum<i32> = IntSum::new(&Sum);
-        sum.update(1).unwrap();
-        sum.update(2).unwrap();
-        sum.update(3).unwrap();
-
-        assert_eq!(sum.total, 6);
+impl Sum {
+    pub fn new() -> Self {
+        Sum { total: 0. }
     }
 
-    #[test]
-    fn message() {
-        let mut sum: IntSum<i32> = IntSum::new(&Sum);
-        sum.update(1).unwrap();
-        sum.update(2).unwrap();
-        sum.update(3).unwrap();
-
-        assert_eq!(sum.messages(1), vec!["    Total: 6"]);
-    }
-
-    #[test]
-    fn sum_empty() {
-        let mean: IntSum<u64> = IntSum::new(&Sum);
-
-        assert_eq!(mean.total, 0);
-    }
-
-    #[test]
-    fn sum_overflow() {
-        let mut sum: IntSum<i8> = IntSum::new(&Sum);
-        sum.update(100).unwrap();
-        sum.update(100).unwrap();
-        sum.update(100).unwrap();
-
-        assert_eq!(sum.total, i8::MAX);
+    fn parse(&self, message: &str) -> f64 {
+        todo!()
     }
 }
 
 #[cfg(test)]
 mod float_tests {
-    use crate::util::aggregators::{
-        aggregator::{AggregationMethod::Sum, Aggregator},
-        sum::FloatSum,
-    };
+    use crate::util::aggregators::{aggregator::Aggregator, sum::Sum};
 
     #[test]
     fn sum() {
-        let mut sum: FloatSum<f32> = FloatSum::new(&Sum);
-        sum.update(1_f32).unwrap();
-        sum.update(2_f32).unwrap();
-        sum.update(3_f32).unwrap();
+        let mut sum: Sum = Sum::new();
+        sum.update("1_f32").unwrap();
+        sum.update("2_f32").unwrap();
+        sum.update("3_f32").unwrap();
 
-        assert!(sum.total - 6_f32 == 0_f32);
+        assert!(sum.total - 6. == 0.);
     }
 
     #[test]
     fn messages() {
-        let mut sum: FloatSum<f32> = FloatSum::new(&Sum);
-        sum.update(1_f32).unwrap();
-        sum.update(2_f32).unwrap();
-        sum.update(3_f32).unwrap();
+        let mut sum: Sum = Sum::new();
+        sum.update("1_f32").unwrap();
+        sum.update("2_f32").unwrap();
+        sum.update("3_f32").unwrap();
 
         assert_eq!(sum.messages(1), vec!["    Total: 6"]);
     }
 
     #[test]
     fn sum_empty() {
-        let mean: FloatSum<f64> = FloatSum::new(&Sum);
+        let mean: Sum = Sum::new();
 
         assert!(mean.total - 0_f64 == 0_f64);
     }
 
     #[test]
     fn sum_overflow() {
-        let mut sum: FloatSum<f64> = FloatSum::new(&Sum);
-        sum.update(f64::MAX).unwrap();
-        sum.update(f64::MAX).unwrap();
+        let mut sum: Sum = Sum::new();
+        sum.update(&format!("{}test", f64::MAX)).unwrap();
+        sum.update(&format!("{} test", f64::MAX)).unwrap();
 
         assert!(sum.total - f64::MAX == 0_f64);
     }

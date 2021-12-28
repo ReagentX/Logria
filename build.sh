18e400a931eb10f8c26b73679c3cbd2cb2986296
Christopher Sardegna <gpg@reagentx.net>
2021-12-27 22:31:13 -0700

    Require valid tags and user confirmation

--- build.sh
+++ build.sh
@@ -7,18 +7,26 @@ git pull
 # Get version number from tag name
 export VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
 
-# Update version number in Cargo.toml for build
-# MacOS sed requires the weird empty string param
-# Otherwise it returns `invalid command code C`
-sed -i '' "s/0.0.0/$VERSION/g" Cargo.toml
+if [ -n "$VERSION" ]; then
+    read -p "Build with version $VERSION? C-c to cancel, any key to continue " -n 1 -r
+    echo
+    # Update version number in Cargo.toml for build
+    # MacOS sed requires the weird empty string param
+    # Otherwise it returns `invalid command code C`
+    sed -i '' "s/0.0.0/$VERSION/g" Cargo.toml
 
-# Build for Apple Silicon
-cargo build --target aarch64-apple-darwin --release
-cp target/aarch64-apple-darwin/release/logria output/logria-aarch64-apple-darwin
+    # Build for Apple Silicon
+    cargo build --target aarch64-apple-darwin --release
+    cp target/aarch64-apple-darwin/release/logria output/logria-aarch64-apple-darwin
 
-# Build for 64-bit Intel MacOS
-cargo build --target x86_64-apple-darwin --release
-cp target/x86_64-apple-darwin/release/logria output/logria-x86_64-apple-darwin
+    # Build for 64-bit Intel MacOS
+    cargo build --target x86_64-apple-darwin --release
+    cp target/x86_64-apple-darwin/release/logria output/logria-x86_64-apple-darwin
 
-# Put the version number back
-sed -i '' "s/$VERSION/0.0.0/g" Cargo.toml
+    # Put the version number back
+    sed -i '' "s/$VERSION/0.0.0/g" Cargo.toml
+    unset VERSION
+else
+    echo 'No version tag set!'
+fi
+

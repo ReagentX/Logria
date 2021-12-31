eb8170a03d9b2e9ad6a1a2ac083562177c231587
Christopher Sardegna <gpg@reagentx.net>
2021-12-30 20:28:05 -0700

    Printout docs if applicable

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,23 @@ mod extensions;
 mod ui;
 mod util;
 
+use communication::reader::main::MainWindow;
+use util::options::from_command_line;
+use constants::cli::messages::DOCS;
+
 fn main() -> Result<()> {
     // Get options from command line
-    let options = util::options::from_command_line();
-    let history = !options.is_present("history");
-    let smart_poll_rate = !options.is_present("smart-poll-rate");
-    let exec: Option<Vec<String>> = options.value_of("exec").map(|text| vec![text.to_string()]);
+    let options = from_command_line();
+    if options.is_present("docs") {
+        println!("{}", DOCS);
+    } else {
+        let history = !options.is_present("history");
+        let smart_poll_rate = !options.is_present("smart-poll-rate");
+        let exec: Option<Vec<String>> = options.value_of("exec").map(|text| vec![text.to_string()]);
 
-    // Start app
-    let mut app = communication::reader::main::MainWindow::new(history, smart_poll_rate);
-    app.start(exec)?;
+        // Start app
+        let mut app = MainWindow::new(history, smart_poll_rate);
+        app.start(exec)?;
+    }
     Ok(())
 }

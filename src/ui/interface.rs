9d39197ce7b7e2071c787f8f66c7019f6585fe52
Christopher Sardegna <gpg@reagentx.net>
2021-12-30 22:34:17 -0700

    Add comments

--- src/ui/interface.rs
+++ src/ui/interface.rs
@@ -7,9 +7,9 @@ fn rect(stdout: &mut Stdout, start: u16, height: u16, width: u16) -> Result<()> 
     for y in start..height {
         for x in 0..width {
             if y == start || y == height - 1 {
-                queue!(stdout, cursor::MoveTo(x, y), style::Print("─"))?;
+                queue!(stdout, cursor::MoveTo(x, y), style::Print("─"))?; // left side
             } else if x == 0 || x == width - 1 {
-                queue!(stdout, cursor::MoveTo(x, y), style::Print("│"))?;
+                queue!(stdout, cursor::MoveTo(x, y), style::Print("│"))?; // right side
             }
             queue!(stdout, cursor::MoveTo(width - 1, start), style::Print("┐"))?; // top right
             queue!(stdout, cursor::MoveTo(0, start), style::Print("┌"))?; // top left
